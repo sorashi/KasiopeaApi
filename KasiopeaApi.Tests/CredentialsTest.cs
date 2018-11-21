435632ca42b1df4574972fdf7d8bfa94f8d105e7
Sorashi <sorashi@protonmail.com>
2018-11-21 23:14:45 +0100

    Add a check for credentials in env to unit tests

---
+++ KasiopeaApi.Tests/CredentialsTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiopeaApi.Tests
+{
+    [TestFixture]
+    class CredentialsTest
+    {
+        [Test]
+        public void CredentialsPresentTest() {
+            if(Credentials.Email == null || Credentials.Password == null) {
+                Assert.Fail("The environment variables kasiopea_email and kasiopea_password are not populated an therefore this project cannot be unit-tested." +
+                    "Please add these env. variables.");
+            }
+        }
+    }
+}
